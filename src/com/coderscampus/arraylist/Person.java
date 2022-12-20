efac65938fcb5f4b585a8e805c38b4a94c9dd1bc
Phil Matthews <phil@viralwebtactics.com>
2022-12-19 21:46:29 -0700

    Refactor POJO for getters & setters

--- src/com/coderscampus/arraylist/Person.java
+++ src/com/coderscampus/arraylist/Person.java
@@ -8,6 +8,14 @@ public class Person {
 		this.name = name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	@Override
 	public String toString() {
 		return "Person [name=" + name + "]";
